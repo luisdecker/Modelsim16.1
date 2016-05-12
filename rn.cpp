3aadb7d8cb67a4c029d0104c78d069fa3e377f9e
Luis Decker <luisgustavo.decker@gmail.com>
2016-05-11 22:55:58 -0300

    Implementação dos geradores de variaveis aleatorias

--- rn.cpp
+++ rn.cpp
@@ -1,7 +1,85 @@
 #include "rn.h"
-
-RN::RN()
-{
-
+//Funcoes da fachada
+//=============================================================================
+double RN::VarAletoria::obterVariavelAleatoria( TipoDist distribuicao,... ) {
+    va_list ap;
+    int numeroArgumentos = obterNumeroArgumentos( distribuicao );
+    va_start( ap,numeroArgumentos );
+    std::vector <double> args;
+    for( int i = 1; i <= numeroArgumentos; i++ ) {
+        args.push_back( va_arg( ap,double ) );
+    }
+    va_end( ap );
+    switch ( distribuicao ) {
+        case constante: {break;}
+        case triangular: { break;}
+        case exponencial: { break;}
+        case normal: { break;}
+        case uniforme: { break;}
+    }
 }
+//=============================================================================
+int RN::VarAletoria::obterNumeroArgumentos( TipoDist distribuicao ) {
+    int numeroArgumentos = 0;
+    switch ( distribuicao ) {
+        case constante: {numeroArgumentos = 1; break;}
+        case triangular: {numeroArgumentos = 3; break;}
+        case exponencial: {numeroArgumentos = 1; break;}
+        case normal: {numeroArgumentos = 2; break;}
+        case uniforme: {numeroArgumentos = 2; break;}
+    }
+    return numeroArgumentos;
+}
+//=============================================================================
+//Geracao de variáveis das distribuicoes
+double RN::Constante::gerarVariavelAleatoria() {
+    return ( double )valor;
+}
+//=============================================================================
+double RN::Triangular::gerarVariavelAleatoria() {
+    double random = ( std::rand()%10000 )/10000;
+    if( random <= ( ( c-a )/( b-a ) ) ) {
+        return a + sqrt( random*( ( c-a )/( b-a ) ) );
+    } else {
+        return b - sqrt( ( 1-random )*( ( b-c )/( b-a ) ) );
+    }
+}
+//=============================================================================
+double RN::Exponencial::gerarVariavelAleatoria() {
+    double random = ( std::rand()%10000 )/10000;
+    return ( -1 )*( media * ( log( 1-random ) ) );
+}
+//=============================================================================
+double RN::Normal::gerarVariavelAleatoria() {
+    if( proximoCalculado ) {
+        proximoCalculado = false;
+        return proximo;
+    }
+    double random1 = ( std::rand()%10000 )/10000, random2 = ( std::rand()%10000 )/10000;
+    double z1 = sqrt( -2 * log( random1 ) ) * cos( 2*M_PI*random2 );
+    double z2 = sqrt( -2 * log( random1 ) ) * sin( 2*M_PI*random2 );
+    proximo = media + ( desvio * z2 );
+    proximoCalculado = true;
+    return ( media + ( desvio * z1 ) );
+}
+//=============================================================================
+double RN::Uniforme::gerarVariavelAleatoria() {
+    double random = ( std::rand()%10000 )/10000;
+    return minimo + ( ( maximo - minimo ) * random );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- rn.h
+++ rn.h
@@ -1,15 +1,69 @@
 #ifndef RN_H
 #define RN_H
+#include <stdarg.h>
+#include <vector>
+#include <cstdlib>
+#include <math.h>
 
+namespace RN {
+    enum TipoDist {constante, triangular, exponencial, normal, uniforme};
+    class VarAletoria {
+    public:
+        static double obterVariavelAleatoria( TipoDist Distribuicao,... );
+    private:
+        static int obterNumeroArgumentos( TipoDist Distribuicao );
+    };
 
-class RN
-{
-public:
-    RN();
+    class Distribuicao {
+    public:
+        Distribuicao();
+        virtual double gerarVariavelAleatoria() = 0;
+        virtual double operator() () = 0;
+    };
 
-signals:
+    class Constante : public Distribuicao {
+    public:
+        Constante ( double valor ): valor( valor ) {}
+        double gerarVariavelAleatoria();
+        double operator ()() {return gerarVariavelAleatoria();}
+    private:
+        double valor;
 
-public slots:
-};
+    };
+    class Triangular : public Distribuicao {
+    public:
+        Triangular( double a, double b, double c ):a( a ), b( b ),c( c ) {}
+        double gerarVariavelAleatoria();
+        double operator ()() {return gerarVariavelAleatoria();}
+    private:
+        double a,b,c;
+    };
+    class Exponencial : public Distribuicao {
+    public :
+        Exponencial( double media ):media( media ) {}
+        double gerarVariavelAleatoria();
+        double operator ()() {return gerarVariavelAleatoria();}
 
+    private:
+        double media;
+    };
+    class Normal : public Distribuicao {
+    public:
+        Normal( double media, double desvio ):media( media ),desvio( desvio ) {proximoCalculado = false;}
+        double gerarVariavelAleatoria();
+        double operator ()() {return gerarVariavelAleatoria();}
+    private:
+        double media,desvio,proximo;
+        bool proximoCalculado;
+    };
+
+    class Uniforme: public Distribuicao {
+    public:
+        Uniforme( double minimo, double maximo ):minimo( minimo ), maximo( maximo ) {}
+        double gerarVariavelAleatoria();
+        double operator ()() {return gerarVariavelAleatoria();}
+    private:
+        double minimo,maximo;
+    };
+}
 #endif // RN_H
